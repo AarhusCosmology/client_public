ee233e764b69a53910e1cae2db1272dead08ab23
LucaJanken <luca1@DESKTOP-QN78HP9.localdomain>
2025-12-14 10:32:16 +0100

    Added parent directory to path for imports in benchmark.py script

--- benchmarking/benchmark.py
+++ benchmarking/benchmark.py
@@ -6,6 +6,9 @@ import argparse
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from pathlib import Path
 from datetime import datetime
 from getdist import MCSamples, plots
