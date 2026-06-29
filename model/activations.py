8387d4b33cefe9113f4ea40f740e95874cbd80a9
LucaJanken <luca1875@live.dk>
2026-06-29 11:40:11 +0200

    changed Alsing parameter initialization from random normal to beta=1 and gamma=0

--- model/activations.py
+++ model/activations.py
@@ -1,30 +1,39 @@
 import tensorflow as tf
 
-@tf.keras.utils.register_keras_serializable()
+@tf.keras.utils.register_keras_serializable(package="CLiENT")
 class Alsing(tf.keras.layers.Layer):
-    """
-    Per-feature Alsing activation: a(x) = (gamma + sigmoid(beta * x) * (1 - gamma)) * x
+    def __init__(self, initial_beta=1.0, initial_gamma=0.0, **kwargs):
+        super().__init__(**kwargs)
+        self.initial_beta = initial_beta
+        self.initial_gamma = initial_gamma
 
-    beta and gamma are learned independently for each input feature.
-    """
     def build(self, input_shape):
         units = int(input_shape[-1])
+
         self.beta = self.add_weight(
             name="beta",
             shape=(units,),
-            initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=1.0),
+            initializer=tf.keras.initializers.Constant(self.initial_beta),
             trainable=True,
         )
+
         self.gamma = self.add_weight(
             name="gamma",
             shape=(units,),
-            initializer=tf.keras.initializers.RandomNormal(mean=0.0, stddev=1.0),
+            initializer=tf.keras.initializers.Constant(self.initial_gamma),
             trainable=True,
         )
 
     def call(self, x):
         return (self.gamma + tf.sigmoid(self.beta * x) * (1.0 - self.gamma)) * x
 
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            "initial_beta": self.initial_beta,
+            "initial_gamma": self.initial_gamma,
+        })
+        return config
 
 def build_activation(name):
     if name == 'alsing':
